15661ad7ec0519258d1208421158f7a3cb783807
sfcc5555 <sfcc.5555@gmail.com>
2023-03-23 15:58:14 -0500

    Pruebas de color

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+      <React.Fragment>
+
+        <h1>Hello world</h1>
+        <h1 className='mF'>Hello world</h1>
+        <h1 className='sF'>Hello world</h1>
+
+      </React.Fragment>
+  );
+}
+
+export default App;
