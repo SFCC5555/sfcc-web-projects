35f40526d6418ab9f7d3b983996fab016a364708
sfcc5555 <sfcc.5555@gmail.com>
2023-03-24 02:52:22 -0500

    actualización NavBar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
       <React.Fragment>
         <NavBar />
-        <h1 className='lightMode'>SFCz</h1>
+        <h1 className='lightMode'>SFCf</h1>
         <img alt='verga' src={require('./assets/icons/sIcon.png')} />
       </React.Fragment>
   );
