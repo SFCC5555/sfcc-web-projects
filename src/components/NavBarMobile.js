47858363bd504b822f715ed77df10c33072aed25
sfcc5555 <sfcc.5555@gmail.com>
2023-03-23 21:40:41 -0500

    actualización NavBar

--- src/components/NavBarMobile.js
+++ src/components/NavBarMobile.js
@@ -6,7 +6,6 @@ function NavBarMobile() {
             <span className="burgerIcon burgerIconLight"></span>
             <div className='sFernando'>ING. S. FERNANDO CARRASCO</div>
             <div className='aboutMe'><span className="sfccPicture"></span>ABOUT ME</div>
-            <div className='aboutMe'><span className="sfccPictureTiny"></span>ABOUT ME</div>
         </nav>
     )
 };
