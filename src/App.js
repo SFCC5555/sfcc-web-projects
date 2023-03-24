93420f931810ee6944c9e8128cf76b5c7bae9d85
sfcc5555 <sfcc.5555@gmail.com>
2023-03-24 03:13:12 -0500

    actualización sfccIcon

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
   return (
       <React.Fragment>
         <NavBar />
-        <h1 className='lightMode'>SFCf</h1>
-        <img alt='verga' src={require('./assets/icons/sIcon.png')} />
+        <img alt='sfcc Icon' src={require('./assets/icons/sfccIcon.png')} />
       </React.Fragment>
   );
 }
