cc2bcd195b10f1089c6dc03d4af80123433602ae
sfcc5555 <sfcc.5555@gmail.com>
2023-03-24 02:38:27 -0500

    actualización NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
     return (
         <nav className="navBar lightModeComponent">
             <span className="burgerIcon burgerIconLight"></span>
-            <a href='https://sfcc-web-projects.netlify.app/' className='sFernando lightModeElement'>ING. S. FERNANDO CARRASCO</a>
+            <a href='.' className='sFernando lightModeElement'>ING. S. FERNANDO CARRASCO</a>
             <div className='aboutMe lightModeElement'><span className="sfccPicture"></span>ABOUT ME</div>
         </nav>
 
