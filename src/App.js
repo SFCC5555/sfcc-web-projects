952c901ac39d6d0efd13ab9cfbab23c3e0a56cfd
sfcc5555 <sfcc.5555@gmail.com>
2023-03-24 10:08:04 -0500

    actualización darkModeButton

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 import React from 'react';
 import { NavBar } from './components/NavBar.js';
 import { DarkModeButton } from './components/DarkModeButton';
+import { useState } from 'react';
 
 function App() {
-  const sectionsList=['WEB PROJECTS','CERTIFICATIONS','CODE NETWORKS','CONTACT','BUY ME A COFFEE']
+  const sectionsList = ['WEB PROJECTS','CERTIFICATIONS','CODE NETWORKS','CONTACT','BUY ME A COFFEE'];
+
+  const body = document.querySelector('body')
+
+  let [mode,setMode] = useState('Light');
+  
+  function controlDarkMode() {
+    mode==='Light'?setMode('Dark'):setMode('Light');
+    body.classList.toggle('lightMode');
+  }
+
+
   return (
       <React.Fragment>
         <NavBar sectionsList={sectionsList}/>
-        <img alt='sfcc Icon' className='sfccIcon' src={require('./assets/icons/sfccIcon.png')} />
-        <DarkModeButton />
+        <img alt='sfcc Icon' className='sfccIcon' src={require(`./assets/icons/sfccIcon${mode}.png`)} />
+        <DarkModeButton controlFunction={controlDarkMode} mode={mode}/>
       </React.Fragment>
   );
 }
--- src/components/DarkModeButton.js
+++ src/components/DarkModeButton.js
@@ -1,10 +1,10 @@
 import '../styles/DarkModeButton.css';
 
 
-function DarkModeButton() {
+function DarkModeButton({ controlFunction, mode }) {
 
     return (
-        <span className='darkModeButton darkModeButtonDark'></span>
+        <span onClick={controlFunction} onMouseOver={controlFunction} onMouseOut={controlFunction} className={`darkModeButton darkModeButton${mode}`}></span>
 
     )
 };
