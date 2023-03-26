dc79b1a1283bb4105df5cf7aec587053b0f5425d
sfcc5555 <sfcc.5555@gmail.com>
2023-03-25 23:31:56 -0500

    skill icons animation actualización

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -13,7 +13,11 @@ function AboutSection({ active, controlFunction, mode}) {
 
             <div onClick={controlFunction} className={`closeIcon ${lowerCaseMode}ModeElement`}>X</div>
             <p className={`${lowerCaseMode}ModeElement`}>A Front-End Developer with entry-level experience specializing in web development, user interface design, HTML, CSS, JavaScript and ReactJS. Adept at identifying opportunities to enhance front-end design and improve the user experience.</p>
-            <Skills skillList={skillList} mode={mode}/>
+            <Skills skillList={skillList} mode={mode} />
+            <section className='links'>
+                <a href='https://github.com/SFCC5555' className={`${lowerCaseMode}ModeElement`}><span className={`skillIcon GitHubIcon${mode}`}></span>GitHub</a>
+                <a href='../assets/documents/CVFernandoCarrasco.pdf' className={`${lowerCaseMode}ModeElement`}><span className={`skillIcon GitHubIcon${mode}`}></span>CV</a>
+            </section>
 
         </section>
 
