ef6e81f12bb039514ac96218fd4c5d61a0c65f8d
sfcc5555 <sfcc.5555@gmail.com>
2023-03-28 03:33:39 -0500

    actualización

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,9 +5,9 @@ function Search( {mode} ) {
     const lowerCaseMode=mode.toLowerCase();
 
     return (
-        <div>
+        <div className='inputCpntainer'>
             <input id='search' className={`${lowerCaseMode}ModeInput searchInput`} type='text' placeholder='search a project' />
-            <label htmlFor='search'><span className={`searchIcon${mode}`} /></label>
+            <label htmlFor='search'><span className={`searchIcon${mode} searchIcon`} /></label>
         </div>
     )
 }
