b5e21f4f2d4343caeed5ad0ee63ffc8424647081
sfcc5555 <sfcc.5555@gmail.com>
2023-03-28 04:33:03 -0500

    actualización

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -5,7 +5,7 @@ function Filter( {mode} ) {
     const lowerCaseMode=mode.toLowerCase();
 
     return (
-        <button className={`${lowerCaseMode}ModeFilter filterButton`}>filter<span className={`filterIcon${mode}`} /></button>
+        <button className={`${lowerCaseMode}ModeFilter filterButton`}>filter<span className={`filterIcon${mode} filterIcon`} /></button>
     )
 }
 
