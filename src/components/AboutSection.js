3c0485424ff1d7552d8ed3c26f001805dbed8aff
sfcc5555 <sfcc.5555@gmail.com>
2023-05-21 01:55:28 -0500

    rock-paper-scissors

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -12,7 +12,7 @@ function AboutSection({ active, controlFunction, mode}) {
         <section className={active?`aboutSection ${lowerCaseMode}ModeComponent`:`aboutSection ${lowerCaseMode}ModeComponent inactive`}>
 
             <div onClick={controlFunction} className={`closeIcon ${lowerCaseMode}ModeElement`}>X</div>
-            <p className={`${lowerCaseMode}ModeElement`}>Front-End Developer with entry-level experience specializing in web development, user interface design, HTML, CSS, JavaScript, and ReactJS. Additionally, I have a keen interest in illustration, and I am constantly looking to broaden my horizons and stay up-to-date with the latest trends and technologies. My goal is to become a Full-Stack Developer, and I am always eager to learn and expand my skillset to achieve this.</p>
+            <p className={`${lowerCaseMode}ModeElement`}>Front-End Developer with experience in web development, user interface design, and technologies such as HTML, CSS, JavaScript, and ReactJS. My goal is to become a Full-Stack Developer, and I am constantly seeking opportunities to expand my skills. I am passionate about problem-solving and creating quality user experiences. I am interested in a position that allows me to develop and enhance my skills while growing professionally.</p>
             <Skills skillList={skillList} mode={mode} noWarp='noWarp'/>
             <section className='links'>
                 <a href='https://github.com/SFCC5555' className={`${lowerCaseMode}ModeElement`}><span className={`skillIcon GitHubIcon${mode}`}></span>GitHub</a>
