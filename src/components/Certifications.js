d625d11d207674fc8c8da42a2e05fd3200161527
sfcc5555 <sfcc.5555@gmail.com>
2023-05-26 09:46:29 -0500

    a

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -9,6 +9,8 @@ function Certifications({mode}) {
 
     let [srcCertification,setSrcCertification] = useState('');
 
+    let [linkCertification,setLinkCertification] = useState('');
+
     let [activeCertification,setActiveCertification] = useState(false);
 
     function closeCertification() {
@@ -19,6 +21,7 @@ function Certifications({mode}) {
         setActiveCertification(false);
         setTimeout(()=>{
             setSrcCertification(event.target.id);
+            setLinkCertification(event.target.attributes.link.value);
             setActiveCertification(true);
         })
     }
@@ -28,16 +31,17 @@ function Certifications({mode}) {
     return (<main className='sectionContainer'>
                 <div className='sectionGap' id='CERTIFICATIONS'></div>
                 <h2 className={`${lowerCaseMode}ModeElement`}>CERTIFICATIONS</h2>
-                <div className={activeCertification?`${lowerCaseMode}ModeComponent renderCertificationContainer`:'inactive'}>
+                {activeCertification&&<div className={`${lowerCaseMode}ModeComponent renderCertificationContainer`}>
                     <div onClick={closeCertification} className={`closeIcon lightModeElement`}>X</div>
+                    <a href={linkCertification[0]==='h'?linkCertification:require(`../assets/documents/${linkCertification}`)} target='_Blank'><span className='externalLinkIcon'/></a>
                     <img src={srcCertification?require(`../assets/images/certificationIllustrations/${srcCertification}Color.png`):''} alt={srcCertification} ></img>
-                </div>
+                </div>}
                 <section className='certificationContainer'>
                     
                     {
                      data.certifications.map(certification=>(
                         <div key={certification.name} className={`${lowerCaseMode}ModeComponent certification`}>
-                            <span onClick={renderCertification} id={certification.name} className={`certificationIllustration`} style={{backgroundImage:`url(${require(`../assets/images/certificationIllustrations/${certification.name}Color.png`)})`}} />
+                            <span onClick={renderCertification} link={certification.link} id={certification.name} className={`certificationIllustration`} style={{backgroundImage:`url(${require(`../assets/images/certificationIllustrations/${certification.name}Color.png`)})`}} />
                         </div>
                      ))   
                     }
