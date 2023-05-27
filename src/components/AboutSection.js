e601152d336a0632bdad1556cc6681b9fbbf3517
sfcc5555 <sfcc.5555@gmail.com>
2023-05-27 02:49:53 -0500

    a

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -15,8 +15,8 @@ function AboutSection({ active, controlFunction, mode}) {
             <p className={`${lowerCaseMode}ModeElement`}>Front-End Developer with experience in web development, user interface design, and technologies such as HTML, CSS, JavaScript, and ReactJS. My goal is to become a Full-Stack Developer, and I am constantly seeking opportunities to expand my skills. I am passionate about problem-solving and creating quality user experiences. I am interested in a position that allows me to develop and enhance my skills while growing professionally.</p>
             <Skills skillList={skillList} mode={mode} noWarp='noWarp'/>
             <section className='links'>
-                <a href='https://github.com/SFCC5555' className={`${lowerCaseMode}ModeElement`}><span className={`skillIcon GitHubIcon${mode}`}></span>GitHub</a>
-                <a href={require('../assets/documents/RESUME2023FERNANDOCARRASCO.pdf')} className={`${lowerCaseMode}ModeElement`}><span className={`skillIcon cvIcon${mode}`}></span>CV</a>
+                <a href='https://github.com/SFCC5555' className={`${lowerCaseMode}ModeElement`}><span className={`skillIcon GitHubIcon${mode}`} target='_Blank' rel="noreferrer" ></span>GitHub</a>
+                <a href={require('../assets/documents/RESUME2023FERNANDOCARRASCO.pdf')} className={`${lowerCaseMode}ModeElement`}><span className={`skillIcon cvIcon${mode}`} target='_Blank' rel="noreferrer" ></span>CV</a>
             </section>
 
         </section>
