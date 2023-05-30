13abdabd6e065ec92e48bad9b35a48bae97a456a
sfcc5555 <sfcc.5555@gmail.com>
2023-05-30 13:20:33 -0500

    a

--- src/index.js
+++ src/index.js
@@ -10,3 +10,18 @@ root.render(
   </React.StrictMode>
 );
 
+/*{
+    "name":"Architecture And Civil Engineering Projects",
+    "link":"https://sfcc-architecture-civil-projects.netlify.app/",
+    "repository":"https://github.com/SFCC5555/Portafolio",
+    "skillList":["Html","Css","JavaScript","AutoCAD","SketchUp"],
+    "info":"This project is an architecture and civil engineering portfolio, developed using HTML, CSS, and JavaScript. CSS Grid Layout was employed to structure the project containers, with media queries for responsive design across various devices. The portfolio includes sections for architecture, civil engineering, and renders, with projects dynamically loaded using JavaScript. Each project showcases an image and name, and clicking on it reveals a popup window with additional details and supplementary files such as blueprints and technical reports. The navigation bar features a dropdown sections menu, and a button that opens the author's curriculum vitae in PDF format in a new tab, allowing visitors to review and download it."
+},*/
+
+/*{
+    "name":"Sfcc Comic And Manga Art",
+    "link":"https://sfccgallery.netlify.app/",
+    "repository":"https://github.com/SFCC5555/GALERIA",
+    "skillList":["Html","Css","JavaScript","Adobe-Illustrator","Clip-Studio-Paint"],
+    "info":"This project consists of an illustration gallery. The layout was created using HTML, while the styling was implemented using CSS. The main structure of the illustration container was achieved through CSS Grid Layout, with a responsive design that automatically adapts to the device it is viewed on, thanks to the use of media queries. The illustrations and their information are loaded dynamically using JavaScript, which also provides interactive features such as a dropdown menu and the ability to view the illustrations in an enlarged size in a separate tab. To add a decorative touch to the page, some illustrations created in Adobe Illustrator and Clip Studio Paint were used."
+},*/
